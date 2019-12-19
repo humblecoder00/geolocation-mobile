93309fc23dd07b73465f6c30b97142f2b75abec4
Şahin Arslan <sahin__arslan@hotmail.com>
2019-12-19 13:57:02 +0100

    initial commit

---
+++ src/components/ActivateLocation.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ActivateLocation = () => {
+  return (
+    <div>
+      <p>Activate Location Options</p>
+    </div>
+  );
+};
+
+export default ActivateLocation;
---
+++ src/components/LocationBox.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+
+class LocationBox extends Component {
+  state = {
+    lat: null,
+    errorMessage: ""
+  };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return (
+        <div className="ui placeholder segment">
+          <div className="ui icon header">
+            <i className="thumbs up icon"></i>
+            Your location is set, lattitude: {this.state.lat}
+          </div>
+        </div>
+      );
+    }
+
+    return (
+      <div className="ui placeholder segment">
+        <div className="ui icon header">
+          <i className="map marker alternate icon"></i>
+          Please give us access to your Geo-location
+        </div>
+        <div className="inline">
+          <button className="ui primary button">Android</button>
+          <button className="ui primary button">Iphone</button>
+        </div>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className="ui sizer vertical segment">
+        <div>{this.renderContent()}</div>
+      </div>
+    );
+  }
+}
+
+export default LocationBox;
